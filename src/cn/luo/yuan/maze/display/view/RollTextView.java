d57897e4ba0aa27cae010ebe8b3584ee4fc4e9bb
luoyuan800 <luoyuan800@163.com>
2017-03-20 22:49:37 +0800

    信息滚动的

--- src/cn/luo/yuan/maze/display/view/RollTextView.java
+++ src/cn/luo/yuan/maze/display/view/RollTextView.java
@@ -1,46 +1,41 @@
 package cn.luo.yuan.maze.display.view;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.PixelFormat;
 import android.text.Html;
 import android.util.AttributeSet;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by luoyuan on 2017/3/19.
  */
-public class RollTextView extends SurfaceView implements SurfaceHolder.Callback {
-    private ArrayDeque<String> infos = new ArrayDeque<>();
-    private List<TextView> caches = new ArrayList<>();
-    public void add(String info){
-        while (infos.size() >= 50){
-            infos.poll();
+public class RollTextView extends ListView {
+    private ArrayDeque<TextView> caches = new ArrayDeque<>(50);
+    private String info;
+    private Runnable addText = new Runnable() {
+        @Override
+        public void run() {
+            TextView tv;
+            if (caches.size() < 50) {
+                tv = new TextView(getContext());
+                tv.setWidth(getWidth() - 5);
+            } else {
+                tv = caches.poll();
+                removeView(tv);
+            }
+
+            tv.setText(Html.fromHtml(info));
+            caches.add(tv);
+            addView(tv);
         }
-        infos.add(info);
-    }
-
-    public void draw(){
-        Canvas canvas = getHolder().lockCanvas(null);
-        canvas.drawColor(Color.argb(0, 0, 0, 0), android.graphics.PorterDuff.Mode.CLEAR);
-        int x = 1,y = 1;
-        for(String info : infos){
-            Html.fromHtml(info);
-
-        }
-
-    }
+    };
 
     public RollTextView(Context context) {
         super(context);
     }
+
 
     public RollTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -50,25 +45,9 @@ public class RollTextView extends SurfaceView implements SurfaceHolder.Callback 
         super(context, attrs, defStyle);
     }
 
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-
+    public synchronized void add(String info) {
+        this.info = info;
+        getHandler().post(addText);
     }
 
-    public void init() {
-        setZOrderOnTop(true);
-        getHolder().addCallback(this);
-        getHolder().setFormat(PixelFormat.TRANSPARENT);
-
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-
-    }
 }
