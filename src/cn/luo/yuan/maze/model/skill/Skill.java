9e108ebafa446289db1e55e4b42c28e76c02c1a3
Gavin Luo <Gavin.Luo@software.dell.com>
2017-04-27 17:40:06 +0800

    增加技能模块

--- src/cn/luo/yuan/maze/model/skill/Skill.java
+++ src/cn/luo/yuan/maze/model/skill/Skill.java
@@ -1,9 +1,15 @@
 package cn.luo.yuan.maze.model.skill;
 
+import cn.luo.yuan.maze.utils.Version;
+
+import java.io.Serializable;
+
 /**
  * Created by gluo on 4/27/2017.
  */
-public interface Skill {
+public interface Skill extends Serializable {
+    long serialVersionUID = Version.SERVER_VERSION;
+
     SkillResult invoke(SkillParameter parameter);
     void enable(SkillParameter parameter);
     boolean canEnable(SkillParameter parameter);
