b82159efe08175d8ee9245d786cb0447150833af
luoyuan800 <luoyuan800@163.com>
2017-06-25 23:41:24 +0800

    交换界面的代码

--- src/cn/luo/yuan/maze/display/dialog/ExchangeDialog.java
+++ src/cn/luo/yuan/maze/display/dialog/ExchangeDialog.java
@@ -1,7 +1,8 @@
 package cn.luo.yuan.maze.display.dialog;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -25,6 +26,73 @@ public class ExchangeDialog {
     private GameContext context;
     private ExchangeManager manager;
     private AlertDialog progress;
+
+    private Handler handler = new Handler(){
+        public void handleMessage(Message msg){
+            switch (msg.what){
+                case 1:
+                    if(msg.obj instanceof List){
+                        List<ExchangeObject> objects = (List<ExchangeObject>) msg.obj;
+                        ExchangeAdapter es = new ExchangeAdapter(objects, new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ExchangeObject eo = (ExchangeObject) v.getTag(R.string.item);
+                                context.getDataManager().save(eo.getExchange());
+                                context.getExecutor().submit(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        manager.getBackMyExchange(eo.getId());
+                                    }
+                                });
+                                ExchangeAdapter adapter = (ExchangeAdapter) v.getTag(R.string.adapter);
+                                adapter.getExchanges().remove(eo);
+                                adapter.notifyDataSetChanged();
+                            }
+                        });
+                        ListView list = new ListView(context.getContext());
+                        list.setAdapter(es);
+                        progress.dismiss();
+                        SimplerDialogBuilder.build(list, Resource.getString(R.string.close), null, context.getContext());
+                    }
+                    break;
+                case 2:
+                    if(msg.obj instanceof List){
+                        List<ExchangeObject> objects = (List<ExchangeObject>) msg.obj;
+                        progress.dismiss();
+                        ExchangeAdapter ea = new ExchangeAdapter(objects, new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                progress.show();
+                                context.getExecutor().submit(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        ExchangeObject eo = (ExchangeObject) v.getTag(R.string.item);
+                                        IDModel myItem = manager.queryMyAvaiableItemForExchange(eo.getExpectedType(), eo.getExpectedKeyWord());
+                                        if (manager.requestExchange((Serializable) myItem, eo)) {
+                                            context.getDataManager().save(eo.getExchange());
+                                            ExchangeAdapter adapter = (ExchangeAdapter) v.getTag(R.string.adapter);
+                                            List<ExchangeObject> exchangeObjects = manager.queryAvailableExchanges(-1);
+                                            adapter.setExchanges(exchangeObjects);
+                                            adapter.notifyDataSetChanged();
+                                            SimplerDialogBuilder.build("交换成功！", Resource.getString(R.string.conform), null, context.getContext());
+                                            progress.dismiss();
+                                        }
+                                    }
+                                });
+
+                            }
+                        });
+                        LinearLayout linearLayout = new LinearLayout(context.getContext());
+                        CheckBox cb = new CheckBox(context.getContext());
+                        linearLayout.addView(cb);
+                        ListView list = new ListView(context.getContext());
+                        list.setAdapter(ea);
+                        linearLayout.addView(list);
+                        SimplerDialogBuilder.build(linearLayout, Resource.getString(R.string.close), null, context.getContext());
+                    }
+            }
+        }
+    };
 
     public ExchangeDialog(GameContext context) {
         this.context = context;
@@ -53,76 +121,37 @@ public class ExchangeDialog {
         linearLayout.setOrientation(LinearLayout.VERTICAL);
         linearLayout.addView(queryItem);
         linearLayout.addView(myItem);
-        SimplerDialogBuilder.build(linearLayout, Resource.getString(R.string.close), null,context.getContext());
+        Button upload = new Button(context.getContext());
+        upload.setText(R.string.upload);
+        //TODO upload
+        linearLayout.addView(upload);
+        SimplerDialogBuilder.build(linearLayout, Resource.getString(R.string.close), null, context.getContext());
     }
 
-    public void showMyExchange() {
+    private void showMyExchange() {
         progress.show();
         context.getExecutor().submit(new Runnable() {
             @Override
             public void run() {
                 List<ExchangeObject> eos = manager.querySubmittedExchangeOfMine();
-                ExchangeAdapter es = new ExchangeAdapter(eos, new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        ExchangeObject eo = (ExchangeObject) v.getTag(R.string.item);
-                        context.getDataManager().save(eo.getExchange());
-                        context.getExecutor().submit(new Runnable() {
-                            @Override
-                            public void run() {
-                                manager.getBackMyExchange(eo.getId());
-                            }
-                        });
-                        ExchangeAdapter adapter = (ExchangeAdapter) v.getTag(R.string.adapter);
-                        adapter.getExchanges().remove(eo);
-                        adapter.notifyDataSetChanged();
-                    }
-                });
-                ListView list = new ListView(context.getContext());
-                list.setAdapter(es);
-                progress.dismiss();
-                SimplerDialogBuilder.build(list, Resource.getString(R.string.close), null, context.getContext());
+                Message msg = new Message();
+                msg.what = 1;
+                msg.obj = eos;
+                handler.sendMessage(msg);
             }
         });
     }
 
-    public void showExchanges() {
+    private void showExchanges() {
         progress.show();
         context.getExecutor().submit(new Runnable() {
             @Override
             public void run() {
                 List<ExchangeObject> exchangeObjects = manager.queryAvailableExchanges(-1);
-                progress.dismiss();
-                ExchangeAdapter ea = new ExchangeAdapter(exchangeObjects, new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        progress.show();
-                        context.getExecutor().submit(new Runnable() {
-                            @Override
-                            public void run() {
-                                ExchangeObject eo = (ExchangeObject) v.getTag(R.string.item);
-                                IDModel myItem = manager.queryMyAvaiableItemForExchange(eo.getExpectedType(), eo.getExpectedKeyWord());
-                                if (manager.requestExchange((Serializable) myItem, eo)) {
-                                    context.getDataManager().save(eo.getExchange());
-                                    ExchangeAdapter adapter = (ExchangeAdapter) v.getTag(R.string.adapter);
-                                    List<ExchangeObject> exchangeObjects = manager.queryAvailableExchanges(-1);
-                                    adapter.setExchanges(exchangeObjects);
-                                    adapter.notifyDataSetChanged();
-                                    SimplerDialogBuilder.build("交换成功！", Resource.getString(R.string.conform), null, context.getContext());
-                                    progress.dismiss();
-                                }
-                            }
-                        });
-
-                    }
-                });
-                LinearLayout linearLayout = new LinearLayout(context.getContext());
-                CheckBox cb = new CheckBox(context.getContext());
-                linearLayout.addView(cb);
-                ListView list = new ListView(context.getContext());
-                list.setAdapter(ea);
-                linearLayout.addView(list);
-                SimplerDialogBuilder.build(linearLayout, Resource.getString(R.string.close), null, context.getContext());
+                Message msg = new Message();
+                msg.what = 2;
+                msg.obj = exchangeObjects;
+                handler.sendMessage(msg);
             }
         });
     }
